6600a0730b9799ac6f358e5552da417f0457a316
Z. D. Smith <cruxdestruct@gmail.com>
2014-03-11 18:02:34 -0400

    Allcaps

--- punc.py
+++ punc.py
@@ -1,17 +1,26 @@
 import zulip
-import sys
+import sys, re
 from collections import Counter
 from pymongo import MongoClient
 from nltk.tokenize import RegexpTokenizer
+import nltk.data
 
 def update_count(text, stream):
     """
     Increment our counter with all the punctuation.
-    >>> update_count('foo!', 'haskell')
     """
-    t  = sanitize_tokens(tokenizer.tokenize(text))
-    associations[stream].update(t)
+    
+    tokens  = map(sanitize_token, tokenizer.tokenize(text))
+    tokens.extend(['ALLCAPS' for t in sent_detector.tokenize(text) if allcaps(t)])
+    associations[stream].update(tokens)
     update_table(stream)
+
+def allcaps(t):
+    allcaps = re.compile("[^a-z]+$")
+    if re.match(allcaps, t):
+        return True
+    else:
+        False
 
 def update_table(counter):
     current_record = {}
@@ -19,34 +28,33 @@ def update_table(counter):
         current_record[element] = associations[counter][element]
     punc_db.update({'name':counter},{"$set": {"PUNC": current_record}}, upsert=True)
 
-def update_db():
-    for counter in associations:
-        update_table(counter)
+def sanitize_token(t):
+    TOKEN_NAMES = {
+     "!" : "BANG"
+    ,"?" : "QUESTION"
+    ,"." : "FULLSTOP"
+    ,":)": "SMILEY"
+    ,":(": "FROWNY" }
+    if t in TOKEN_NAMES.keys():
+        return TOKEN_NAMES[t]
+    else:
+        return t
 
-def sanitize_tokens(t):
-    for index, token in enumerate(t):
-        if token == '.':
-            t[index] = 'FULLSTOP'
-        elif token == '?':
-            t[index] = 'QUESTION'
-        elif token == '!':
-            t[index] = 'BANG'
-    return t
-
-# response = client.register(event_types=["messages"])
 def print_and_add(message):
     content = message['content']
     stream = message['display_recipient']
     print message['sender_short_name'] + ": " + content 
     update_count(content, stream)
+    print associations[stream]
+
 
 client = zulip.Client(email="punc-bot@students.hackerschool.com",
                     api_key="7rZVIQkv5XIAEZYOWW61imFZdSieAeGQ")
-
 db_connection = MongoClient('localhost', 27017)
 db = db_connection[__name__]
 punc_db = db.punc
-tokenizer = RegexpTokenizer('[?!.]')
+tokenizer = RegexpTokenizer('[?!.]|:\)|:\(')
+sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 associations = {}
 
 for sub in client.list_subscriptions()['subscriptions']:
@@ -56,11 +64,15 @@ for sub in client.list_subscriptions()['subscriptions']:
     
     db_record = punc_db.find_one({'name':stream})
     punc_record = db_record.get('PUNC') or {}
-    associations[stream] = Counter(punc_record)    
+    associations[stream] = Counter(punc_record) 
+
+def run():
+    client.call_on_each_message(print_and_add)
+
+      
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
-    client.call_on_each_message(print_and_add)
-
-# client.do_api_query({'anchor':'18202209', 'num_before':'100', 'num_after':'0', 'narrow': [['stream', 'python']]}, event-types=["messages"], 'https://zulip.com/api/v1/messages', method='GET')
+    run()
+    
